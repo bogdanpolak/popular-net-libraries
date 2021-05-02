a4ffb43a35a94be236631c79d4bb5c4f1f1b3ab4
Bogdan Polak <bogdanpol@gmail.com>
2021-05-02 22:10:15 +0200

    Added InvalidMapping

--- src/PopularNetLibraries/Automapper/BasicMapper.cs
+++ src/PopularNetLibraries/Automapper/BasicMapper.cs
@@ -3,14 +3,13 @@ using Xunit;
 
 namespace PopularNetLibraries.Automapper
 {
-    class Order { public int OrderId; }
-
-    class OrderDto { public int OrderId; }
-
     public class BasicMapper
     {
+        private class Order { public int OrderId; }
+        private class OrderDto { public int OrderId; }
+        
         [Fact]
-        public void Test01()
+        public void BasicMapping()
         {
             var config = new MapperConfiguration(expression => expression.CreateMap<Order, OrderDto>());
             var mapper = config.CreateMapper();
@@ -18,7 +17,33 @@ namespace PopularNetLibraries.Automapper
             var order = new Order {OrderId = 99};
             OrderDto orderDto = mapper.Map<OrderDto>(order);
             
-            Assert.Equal(99, orderDto.OrderId);
+            Assert.True(orderDto.OrderId == order.OrderId);
         }
+        private class Source { public int Value; }
+        private class Destination { public int Valueee; }
+
+        [Fact]
+        public void InvalidMapping()
+        {
+            var config = new MapperConfiguration(expression => expression.CreateMap<Source, Destination>());
+            var mapper = config.CreateMapper();
+            
+            var exception = Assert.Throws<AutoMapperConfigurationException>( 
+                ()=>config.AssertConfigurationIsValid() );
+            /*
+            Unmapped members were found. Review the types and members below.
+            Add a custom mapping expression, ignore, add a custom resolver, or modify the source/destination type
+            For no matching constructor, add a no-arg ctor, add optional arguments, or map all of the constructor parameters
+            ============================================================================================================
+            Source -> Destination (Destination member list)
+            PopularNetLibraries.Automapper.BasicMapper+Source -> PopularNetLibraries.Automapper.BasicMapper+Destination (Destination member list)
+
+            Unmapped properties:
+            Valueee
+             */
+            Assert.Contains("Unmapped members were found.",exception.Message);
+            Assert.Contains("Unmapped properties:\nValueee",exception.Message);
+        } 
     }
+    
 }
