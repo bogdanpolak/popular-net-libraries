d14dd635c8e5790a501622cbfe67aa5237a96460
Bogdan Polak <bogdanpol@gmail.com>
2021-05-03 10:57:05 +0200

    Added template for mapper projections

---
+++ src/PopularNetLibraries/Automapper/MapperProjectionTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace PopularNetLibraries.Automapper
+{
+    public class MapperProjectionTests
+    {
+        [Fact]
+        public void EventProjection()
+        {
+            // TODO: AutoMapper projection sample
+            // https://docs.automapper.org/en/latest/Projection.html
+            // https://stackoverflow.com/questions/48910024/automapper-converting-string-to-enum
+        }
+    }
+}
