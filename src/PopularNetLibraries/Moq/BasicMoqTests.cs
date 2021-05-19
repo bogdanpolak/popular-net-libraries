e48f74427b7ffd15291bcaf0908d933e0c614040
Bogdan Polak <bogdanpol@gmail.com>
2021-05-19 23:04:45 +0200

    Additional basic mocks

--- src/PopularNetLibraries/Moq/BasicMoqTests.cs
+++ src/PopularNetLibraries/Moq/BasicMoqTests.cs
@@ -52,18 +52,56 @@ namespace PopularNetLibraries.Moq
             }
 
             [Fact]
-            public void CountProcessedd_LazyCalculateResult()
+            public void CountProcessed_LazyCalculateResult()
             {
                 var count = 1;
                 var assetProcessorMock = new Mock<IAssetProcessor>();
                 assetProcessorMock.Setup(foo => foo.CountProcessed()).Returns(() => count);
+                
                 count += 99;
-
                 var actual = assetProcessorMock.Object.CountProcessed();
+                
                 Assert.Equal(100, actual);
             }
             // TODO: Add Async method to IAssetProcessor
         }
 
+        public class ConverterMock
+        {
+            public interface IConverter
+            {
+                string ConvertName(string name);
+                bool TryParse(string value, out int outputValue);
+            }
+
+            private const string SuperSessions = "super-sessions";
+
+            [Fact]
+            public void TryParse_WillReturnLength()
+            {
+                var converterMock = new Mock<IConverter>();
+                var length = SuperSessions.Length;
+                converterMock.Setup(converter => converter.TryParse(SuperSessions, out length))
+                    .Returns(true);
+
+                var result = converterMock.Object.TryParse(SuperSessions, out var actualLength);
+
+                Assert.True(result);
+                Assert.Equal(14, actualLength);
+            }
+            
+            [Fact]
+            public void ConvertName_DefineDynamicBehaviour()
+            {
+                var converterMock = new Mock<IConverter>();
+                converterMock.Setup(converter => converter.ConvertName(It.IsAny<string>()))
+                    .Returns((string s) => s.ToLower());
+
+                var actual = converterMock.Object.ConvertName("Bogdan Polak");
+                
+                Assert.Equal("bogdan polak", actual);
+            }
+
+        }
     }
 }
