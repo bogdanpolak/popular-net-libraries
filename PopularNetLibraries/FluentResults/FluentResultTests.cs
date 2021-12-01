b091ac42221a82e5d4f1d89a49b6df0503ef193b
Bogdan Polak <bogdanpol@gmail.com>
2021-12-01 23:01:39 +0100

    Added tests for Result.Try()

--- PopularNetLibraries/FluentResults/FluentResultTests.cs
+++ PopularNetLibraries/FluentResults/FluentResultTests.cs
@@ -30,5 +30,36 @@ namespace PopularNetLibraries.FluentResults
             result.IsSuccess.Should().BeFalse();
             result.Reasons.Should().HaveCount(5);
         }
+
+        [Fact]
+        public void Result_Try_WithFail()
+        {
+            var resultFailed = Result.Try(() => Converters.ToTemperature("110"));
+            resultFailed.IsFailed.Should().BeTrue();
+            var error = resultFailed.Errors[0] as ExceptionalError;
+            if (error is null) throw new Exception("Invalid error");
+            error.Exception.Should().BeAssignableTo<ArgumentOutOfRangeException>();
+            error.Exception.Message.Should().Be("Out of temperature range (-150..100) (Parameter 'text')");
+
+        }
+
+        [Fact]
+            public void Result_Try_WithSuccess()
+            {
+            var resultSuccess = Result.Try(() => Converters.ToTemperature("-10"));
+            resultSuccess.ValueOrDefault.Should().Be(-10);
+        }
+    }
+
+    public static class Converters
+    {
+        public static int ToTemperature(string text)
+        {
+            if (!int.TryParse(text, out var value))
+                throw new ArgumentException("Not a number", nameof(text));
+            if (value is <= -150 or >= 100)
+                throw new ArgumentOutOfRangeException(nameof(text), "Out of temperature range (-150..100)");
+            return value;
+        }
     }
 }
